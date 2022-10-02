cdedbe0e4c6ac2e9ac14865e057ba708a8d46d6a
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-10-02 20:44:05 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,75 @@
+//https://practice.geeksforgeeks.org/problems/evaluation-of-postfix-expression/0
+
+//{ Driver Code Starts
+// C++ program to evaluate value of a postfix expression
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to evaluate a postfix expression.
+    int evaluatePostfix(string S)
+    {
+        stack<int> st;
+        for(int i=0;i<S.length();i++)
+        {
+            if(S[i]>47 && S[i]<=57)
+            {
+                st.push(S[i]-'0');
+                // converts the character at position i to
+                //a numeric digit
+            }
+            else
+            {
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                switch(S[i])
+                {
+                    case '+':
+                        st.push(b+a);
+                        break;
+                    case '-':
+                        
+                        st.push(b-a);
+                        break;
+                    case '*':
+                        st.push(b*a);
+                        break;
+                    case '/':
+                        st.push(b/a);
+                        break;
+                }
+                
+            }
+        }
+        return st.top();
+        // Your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+    
+    cout<<obj.evaluatePostfix(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
